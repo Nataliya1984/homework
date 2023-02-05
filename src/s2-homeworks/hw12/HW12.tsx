cf8a20a42c93852bbc2fc367b7cdcff059858b42
Natalya1984 <lisa.volma1984@gmail.com>
2023-02-05 23:06:51 +0300

    lesson_12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -4,6 +4,7 @@ import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
 import {changeThemeId} from './bll/themeReducer'
+import {AppStoreType} from "../hw10/bll/store";
 
 /*
 * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
@@ -20,10 +21,11 @@ const themes = [
 
 const HW12 = () => {
     // взять ид темы из редакса
-    const themeId = 1
+    const themeId = useSelector<AppStoreType, number>(state => state.theme.themeId)
+    const dispatch = useDispatch()
 
-    const change = (id: any) => { // дописать функцию
-
+    const change = (id: number) => { // дописать функцию
+        dispatch(changeThemeId(id))
     }
 
     useEffect(() => {
@@ -36,12 +38,14 @@ const HW12 = () => {
                 Homework #12
             </div>
 
-            <div className={s2.hw}>
+            <div className={s2.hw} style={{padding:'20px 70px'}}>
+                <p>Выберите тему</p>
                 <SuperSelect
                     id={'hw12-select-theme'}
                     className={s.select}
                     // сделать переключение тем
-
+                    onChangeOption={change}
+                    options={themes}
                 />
             </div>
         </div>
